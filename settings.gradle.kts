a20901cabc306cf04acf8e46b7dfec244f940330
Shazz <u202114900@upc.edu.pe>
2026-01-05 04:19:05 -0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "upc-pre-202401-cc238-EA-LucasCoronel_NadiaAlessandra_u202120430"
+include(":app")
+ 
